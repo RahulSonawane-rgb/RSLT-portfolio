e0b7010c95b4ecb94db13768e3659bfa2493f943
Rahul Sonawane <sorahul54@gmail.com>
2026-01-11 20:20:45 +0530

    Update reels.ts

--- src/data/reels.ts
+++ src/data/reels.ts
@@ -1,3 +1,4 @@
 export const reelLinks = [
-  "https://www.instagram.com/reel/DTVOmARExu9/?igsh=MTdzamczczBwajA0"
+  "https://www.instagram.com/reel/DTVOmARExu9/?igsh=MTdzamczczBwajA0",
+"https://www.instagram.com/reel/DTX0XRjCM2G/?igsh=MWdjZWo4YTJqaWpvdQ=="
 ];

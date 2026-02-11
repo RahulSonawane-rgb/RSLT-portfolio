d8cf9f8e012fb79c8edc9d895c76da7033f83252
Rahul Sonawane <sorahul54@gmail.com>
2026-02-12 01:28:44 +0530

    Update reels.ts

--- src/data/reels.ts
+++ src/data/reels.ts
@@ -1,4 +1,6 @@
 export const reelLinks = [
+  "https://www.instagram.com/reel/DUno5ZpEtxW/?igsh=MTMwYWpyOHZsbXU1ag==",
+  "https://www.instagram.com/reel/DUlGaZHEp9J/?igsh=MWxyYnJlYzh5Y2c4YQ==",
   "https://www.instagram.com/reel/DUN4p14jCxz/?igsh=MW9hejk5Z2p1eGlwcA==",
   "https://www.instagram.com/reel/DTmU2AAE9HL/?igsh=cHJmN2FxbmJreHo5",
   "https://www.instagram.com/reel/DTc-OKEk1ky/?igsh=MXM4Y3l1azM0d3dyNg==",

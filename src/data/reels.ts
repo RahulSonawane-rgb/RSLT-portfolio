a6df99c9bfe63b348334759e299ac010e911b677
Rahul Sonawane <sorahul54@gmail.com>
2026-03-04 11:07:11 +0530

    Update reels.ts

--- src/data/reels.ts
+++ src/data/reels.ts
@@ -1,9 +1,9 @@
 export const reelLinks = [
+  "https://www.instagram.com/reel/DU-4PiujG0G/?igsh=M2owbmVhaHA0ZWtz",
+  "https://www.instagram.com/reel/DU8NrBeksdH/?igsh=aTg5Y2RlMWIwaThn",
   "https://www.instagram.com/reel/DUno5ZpEtxW/?igsh=MTMwYWpyOHZsbXU1ag==",
   "https://www.instagram.com/reel/DUlGaZHEp9J/?igsh=MWxyYnJlYzh5Y2c4YQ==",
   "https://www.instagram.com/reel/DUN4p14jCxz/?igsh=MW9hejk5Z2p1eGlwcA==",
   "https://www.instagram.com/reel/DTmU2AAE9HL/?igsh=cHJmN2FxbmJreHo5",
-  "https://www.instagram.com/reel/DTc-OKEk1ky/?igsh=MXM4Y3l1azM0d3dyNg==",
-  "https://www.instagram.com/reel/DTVOmARExu9/?igsh=MTdzamczczBwajA0",
-  "https://www.instagram.com/reel/DTX0XRjCM2G/?igsh=MWdjZWo4YTJqaWpvdQ=="
+  "https://www.instagram.com/reel/DTc-OKEk1ky/?igsh=MXM4Y3l1azM0d3dyNg=="
 ];
